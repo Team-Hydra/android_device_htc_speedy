9acbedbb69aab4d6540748c2205a4e551fd48103
andrew.boren <andrew.boren@gmail.com>
2013-05-23 21:22:56 -0700

    speedy: fstab.* support and cleanup.Update ramdisk.
     * Along with encryptable data partition.
    
    Change-Id: I55527297c3c4395c4d580fc12506814a46947e16

--- full_speedy.mk
+++ full_speedy.mk
@@ -25,6 +25,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     device/htc/speedy/prebuilt/root/init.speedy.rc:root/init.speedy.rc \
+    device/htc/speedy/prebuilt/root/fstab.speedy:root/fstab.speedy \
     device/htc/speedy/prebuilt/root/init.speedy.usb.rc:root/init.speedy.usb.rc \
     device/htc/speedy/prebuilt/root/ueventd.speedy.rc:root/ueventd.speedy.rc
 
